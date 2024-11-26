69d9e84b12b95fc3dc6bd8165c67653eafa3d45d
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 15:31:00 +0100

    frontend:feat: add sort by asteroid name functionality

--- frontend/src/helpers/helpers.tsx
+++ frontend/src/helpers/helpers.tsx
@@ -1,4 +1,4 @@
-import { DateRange } from "../@types/types"
+import { DateRange, Asteroid } from "../@types/types"
 
 export const currentDate = ():string => {
   const date = new Date()
@@ -17,7 +17,32 @@ export const isValidDateRange = (dateRange:DateRange) => {
 
 export const isDateRangeWithinAWeek = ({startDate, endDate}: DateRange) => {
   const daysInAWeek = 7
-  let daysOfDifference =(new Date(endDate).getTime() - new Date(startDate).getTime()) / 1000
-  daysOfDifference /= (60 * 60 * 24) // converts from milliseconds to days
-  return Math.abs(Math.round(daysOfDifference)) <= daysInAWeek
+  const secondsPerHour = 3600
+  const hoursPerDay = 24
+  // difference of days in seconds
+  let timeDifference =(new Date(endDate).getTime() - new Date(startDate).getTime()) / 1000
+  // converts from seconds to days
+  timeDifference /= (secondsPerHour * hoursPerDay) 
+  return Math.abs(Math.round(timeDifference)) <= daysInAWeek
+}
+
+export const sortAsteroidsByName = (asteroids: Asteroid[], setter: (asteroid: Asteroid[]) => void) => {
+  if (!asteroids) return;
+
+  const sortedAsteroids = [...asteroids];
+  sortedAsteroids.sort((a, b) => {
+    const partsNameA = a.name.split(' ', 2)
+    const partsNameB = b.name.split(' ', 2)
+
+    const [digitsA, restA] = [parseInt(partsNameA[0], 10), partsNameA[1]]
+    const [digitsB, restB] = [parseInt(partsNameB[0], 10), partsNameB[1]]
+
+    if (digitsA === digitsB) {
+      return restA.localeCompare(restB)
+    }
+
+    return digitsA - digitsB
+  });
+
+  setter(sortedAsteroids)
 }
