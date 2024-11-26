56b1315d1f8168fe8a4be05fc4a4c6c0ea943240
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 14:50:36 +0100

    backend:feat add full date validation input

--- backend/src/controllers/asteroidController.ts
+++ backend/src/controllers/asteroidController.ts
@@ -1,6 +1,12 @@
 import asyncHandler from "express-async-handler";
 import dotenv from "dotenv";
-import { currentDate, processAsteroids } from "../utils/helpers";
+import { 
+  currentDate, 
+  processAsteroids,
+  isValidDateFormat,
+  isValidDateRange, 
+  isDateRangeWithinAWeek 
+} from "../utils/helpers";
 
 dotenv.config()
 
@@ -13,6 +19,21 @@ const API_KEY = process.env.API_KEY
 export const getAsteroids = asyncHandler( async(req, res) => {
   const startDate = req.query.startDate as string || currentDate()
   const endDate = req.query.endDate as string || currentDate()
+
+  if (!isValidDateFormat(startDate) || !isValidDateFormat(endDate)) {
+    res.status(400)
+    throw new Error('Invalid format date: must be YYYY-MM-DD')
+  }
+
+  if (!isValidDateRange(startDate, endDate)) {
+    res.status(400)
+    throw new Error('Start date must be earlier or equal to the end date!')
+  }
+
+  if (!isDateRangeWithinAWeek(startDate, endDate)) {
+    res.status(400)
+    throw new Error('Date ranges greater than a week are not allowed!')
+  }
 
   const completeURL = 
     `${ASTEROID_API_URL}?start_date=${startDate}&end_date=${endDate}&detailed=false&api_key=${API_KEY}`
