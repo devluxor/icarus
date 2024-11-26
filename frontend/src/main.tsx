412e127082fba3a40583aef600f92dc86421c02d
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 15:20:32 +0100

    frontend: style add basic stylings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './styles/reset.css'
+import './styles/titus-system.css'
+import './styles/app.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
