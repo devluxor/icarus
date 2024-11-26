57f27f46f8d1da827dde0cceab08a7a7e4d126c7
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 15:06:10 +0100

    frontend: feat add App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react"
+import { Asteroid, DateRange } from "./@types/types";
+import { currentDate } from "./helpers/helpers";
+import { getAsteroids } from "./services/asteroidAPI";
+
 
 function App() {
+  const [asteroids, setAsteroids] = useState<Asteroid[] | null>(null);
+  const [dateRange, setDateRange] = useState<DateRange>({startDate: currentDate(), endDate: currentDate() })
+
+  useEffect(() => {
+    const loadAsteroids = async () => {
+      if (asteroids) {
+        setAsteroids(null)
+      }
+      const asteroidsRaw = await getAsteroids(dateRange)
+      setAsteroids(asteroidsRaw)
+    }
+
+    loadAsteroids()
+  }, [dateRange])
+
   return (
     <div className="main-container">
-      Icarus
+      <h1 className="title">🌠Icarus</h1>
+      <h2 className="subtitle">Find your favourite asteroids!</h2>
     </div>
   )
 }
