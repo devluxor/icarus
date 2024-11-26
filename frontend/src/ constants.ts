0f13a0b27f58f6b396d4fbe358143e64839d1b87
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 14:59:52 +0100

    frontend: chore add frontend scaffolding and setup

---
+++ frontend/src/ constants.ts
@@ -0,0 +1,5 @@
+const config = {
+  SERVER_URL: 'http://localhost:8080/api/asteroids'
+};
+
+export default config;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="main-container">
+      Icarus
+    </div>
+  )
+}
+
+
+
+
+export default App
