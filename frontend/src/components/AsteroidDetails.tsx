1c33bc8c5ff564a61b7a14b4e3ae35160abfda1b
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 16:03:29 +0100

    frontend:feat fetch asteroid details from API within modal

--- frontend/src/components/AsteroidDetails.tsx
+++ frontend/src/components/AsteroidDetails.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react"
 import { Asteroid } from "../@types/types"
+import { getAsteroidDetails } from "../services/asteroidAPI"
 
 type AsteroidDetailsProps = {
   asteroid: Asteroid,
@@ -6,11 +8,29 @@ type AsteroidDetailsProps = {
 }
 
 export function AsteroidDetails({asteroid, toggleOverlay}: AsteroidDetailsProps) {
+  const [asteroidDetails, setAsteroidDetails] = useState<any>(null)
 
+  useEffect(() => {
+    const loadAsteroidDetails = async () => {
+      const asteroidDetails = await getAsteroidDetails(asteroid.id)
+      setAsteroidDetails(asteroidDetails)
+    }
+    loadAsteroidDetails()
+  }, [])
 
   return (
     <div className="asteroid-details" onClick={(e) => e.stopPropagation()}>
       <h2>{asteroid.id}</h2>
+
+      {(
+        asteroidDetails &&
+        <>
+          <p>{asteroidDetails.name}</p>
+          <p>🌠</p>
+          <a href={asteroidDetails.nasaURL} target="_blank">Link to NASA detailed page</a>
+        </> 
+       ) || <p>Asteroid loading...</p>}
+
       <button onClick={toggleOverlay}>Close</button>
     </div>
   )
