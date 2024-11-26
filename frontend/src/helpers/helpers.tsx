ec3f8caace0c4203764f9335efb22abeca9fdff5
Lucas Sorribes <lucas@luxor.dev>
2024-11-26 15:13:15 +0100

    frontend:feat add date helpers

--- frontend/src/helpers/helpers.tsx
+++ frontend/src/helpers/helpers.tsx
@@ -1,5 +1,23 @@
+import { DateRange } from "../@types/types"
+
 export const currentDate = ():string => {
   const date = new Date()
   const formattedDate = date.toISOString().split('T')[0]
   return formattedDate
+}
+
+export const isValidDateRange = (dateRange:DateRange) => {
+  const {startDate, endDate} = dateRange
+
+  const epochStartDate = new Date(startDate).getTime()
+  const epochEndDate = new Date(endDate).getTime()
+
+  return epochStartDate <= epochEndDate
+}
+
+export const isDateRangeWithinAWeek = ({startDate, endDate}: DateRange) => {
+  const daysInAWeek = 7
+  let daysOfDifference =(new Date(endDate).getTime() - new Date(startDate).getTime()) / 1000
+  daysOfDifference /= (60 * 60 * 24) // converts from milliseconds to days
+  return Math.abs(Math.round(daysOfDifference)) <= daysInAWeek
 }
